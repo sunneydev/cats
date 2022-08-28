55d47cfaf1819337a7c66029e3860caa6398bd9f
sunney-x <sanishubitidze@live.com>
2022-08-28 16:01:18 +0400

    moved Results to a separate component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
-import type { TResults, Vote } from "./types";
-import { useState } from "react";
-import { getResults, image } from "./api";
-import Card from "./Card";
+import type { TResults } from "./types";
+import { useEffect, useState } from "react";
+import { getResults } from "./api";
+import Results from "./components/Results";
+import Card from "./components/Card";
 
 function App() {
   const [showResults, setShowResults] = useState(false);
@@ -12,72 +13,21 @@ function App() {
       "error" in res ? alert(res.error) : setResults(res)
     );
 
+  useEffect(() => {
+    if (showResults) fetchResults();
+  }, [showResults]);
+
   return (
     <div
       className={`bg-black md:h-screen w-full flex flex-col items-center justify-center p-10`}
     >
-      {showResults && results ? (
-        <div className="h-screen">
-          <div className="flex flex-col gap-10">
-            <div className="flex flex-col">
-              <div className="text-white text-center text-2xl font-semibold mb-10">
-                Best 😊
-              </div>
-              <div className="flex flex-col md:flex-row gap-4">
-                {results.best.map((result) => (
-                  <div>
-                    <img
-                      src={image(result.name)}
-                      alt="cat"
-                      width={200}
-                      height={300}
-                      className="object-center max-h-[500px] rounded-3xl"
-                    />
-                    <div className="text-white text-center text-lg font-semibold">
-                      {result.votes} votes
-                    </div>
-                  </div>
-                ))}
-              </div>
-            </div>
-            <div className="flex flex-col">
-              <div className="text-white text-center text-2xl font-semibold mb-10">
-                Worst 😠
-              </div>
-              <div className="flex md:flex-row md:gap-4">
-                {results.worst.map((result) => (
-                  <div>
-                    <img
-                      src={image(result.name)}
-                      alt="cat"
-                      width={200}
-                      height={300}
-                      className="object-center max-h-[500px] rounded-3xl"
-                    />
-                    <div className="text-white text-center text-lg font-semibold">
-                      {result.votes} votes
-                    </div>
-                  </div>
-                ))}
-              </div>
-            </div>
-          </div>
-        </div>
-      ) : (
-        <Card />
-      )}
       <button
-        onClick={() => {
-          setShowResults((prev) => {
-            if (!prev) fetchResults();
-
-            return !prev;
-          });
-        }}
+        onClick={() => setShowResults((prev) => !prev)}
         className="absolute right-0 top-0 m-4 text-black py-2 px-4 font-semibold bg-white rounded-xl hover:bg-zinc-800 hover:text-white transition-colors duration-200"
       >
         {showResults ? "Play" : "Results"}
       </button>
+      {showResults && results ? <Results results={results} /> : <Card />}
     </div>
   );
 }
